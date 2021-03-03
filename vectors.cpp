8907c945b2da4d7485c7b03a61ae59c2060253c4
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-03-04 04:21:17 +0530

    .

--- vectors.cpp
+++ vectors.cpp
@@ -7,22 +7,30 @@
  using std::vector; //have to use the standard namespace 
 
 int main(){
-
+vector <char> vowel; // empty vector initialized at 0(vectors automatically initialize at 0).
 vector <char> vowels {'a','e'};
 vector <int> test_scores (100,0); //100 ints initialized at 0. 
-vector <int> testScore {100, 50, 40, 30, 10};
+vector <int> testScore {100, 50, 40, 30, 10}; //all are initialized. 
 
-cout<<"The first score is: "<<testScore[0]<<endl;
-cout<<"The second score is: "<<testScore.at(1)<<endl;
+cout<<"The first score is: "<<testScore[0]<<endl; //using the array syntax(not bound checking)
+cout<<"The second score is: "<<testScore.at(1)<<endl; 
+//using the "at" method and dot(.) operator (this one is bound checked)
 
-testScore.at(1) = 90;
+testScore.at(1) = 90; // assigning vector element some other value
 
 cout<<"The second score is: "<<testScore.at(1)<<endl;
 
 testScore.push_back(50); //"push_back" methods adds an desired element at the last of the vector...
 
-for(int i {0}; i<6; i++){
+for(int i {0}; i < 6; i++){
 
+  cout<<testScore.at(i)<<endl;
+}
+
+cout<<"Size of the vector testScore: "<<testScore.size()<<endl;
+
+for(int i {0}; i < testScore.size(); i++){
+// using the "size" method to iterate through the vector.
   cout<<testScore.at(i)<<endl;
 }
 
