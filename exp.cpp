c60c94117c242b367d13c1b96980a5ed6ef8012a
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-03-07 16:29:02 +0530

    .

--- exp.cpp
+++ exp.cpp
@@ -1,19 +1,36 @@
-#include<iostream>
+#include <iostream>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 
+int dollar_conversion()
+{
 
-int learning_exp(){
-; //this a null statement
+    const double inr_per_usd{75.0};
+    double dollars{0.0};
 
-return 1;
+    cout << "\n========== Welcome to USD to INR converter ===========" << endl;
+    cout << "Enter the value in USD: ";
+    cin >> dollars;
+
+    return dollars * inr_per_usd;
 }
 
-int main(){
+int main()
+{
 
-learning_exp();
+    int num1{10}, num2{20}; //this is initialisation and not assingment
 
-return 0;
+    num1 = 100; //here we're not saying num1 is equal to 100 but
+                //we're assigning num1 the value of 100 to store at the location of num1
+
+    cout << num1 << endl
+         << num2;
+
+    num2 = dollar_conversion();
+
+    cout << "\nThe amount in INR is: " << num2 << endl;
+
+    return 0;
 }
