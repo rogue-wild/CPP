bfac86253d249efc9fbbc7f20c4dbcde31c8f19e
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-03-08 20:26:17 +0530

    .

--- exp.cpp
+++ exp.cpp
@@ -6,7 +6,6 @@ using std::endl;
 
 int dollar_conversion()
 {
-
     const double inr_per_usd{75.0};
     double dollars{10.0};
 
@@ -19,10 +18,9 @@ int dollar_conversion()
 
 int main()
 {
-
     int num1{10}, num2{20}; //this is initialisation and not assingment
 
-    num1 = 100; //here we're not saying num1 is equal to 100 but
+     num1 = 100; //here we're not saying num1 is equal to 100 but
                 //we're assigning num1 the value of 100 to store at the location of num1
 
     cout << num1 << endl
@@ -59,6 +57,32 @@ int main()
     cout << "diplaying num1 and num2 using post increment" << endl;
     cout << num1 << endl
          << num2 << endl;
+
+    // Explicit type casting conversion using static_cast<type>
+    cout << "using Explicit Type Casting." << endl;
+    int total_amount{100};
+    int total_number{8};
+    double average{0.0};
+
+    average = total_amount / total_number;
+    cout << average << endl;
+
+    average = static_cast<double>(total_amount) / total_number; // using Explicit Type Casting.
+    cout << average << endl;
+
+    // bool and boolalpha
+    bool result{0};
+
+    cout << std::boolalpha
+         << result << endl;
+
+    cout << std::noboolalpha
+         << result << endl;
+
+    if (num1 <= 10 && num2 <= 20)
+    {
+        cout << "OK" << endl;
+    }
 
     return 0;
 }
