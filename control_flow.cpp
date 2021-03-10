538ea98238b5477b66c82cd551171219cf46f170
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-03-10 22:59:55 +0530

    .

--- control_flow.cpp
+++ control_flow.cpp
@@ -1,10 +1,35 @@
-#include<iostream>
+#include <iostream>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 
-int main(){
+int main()
+{
     //learning Control Flow statements.
+
+    //Using if Statement
+
+    //Using if-else Statement
+
+    //Using Nested if Statement
+
+    //switch-case Statement
+
+    //Conditional Operator
+
+    //Looping
+
+    //for Loop
+
+    //while Loop
+
+    //do while Loop
+
+    //continue and break
+
+    //Infinite Loop
+
+    //Nested Loops
     return 0;
 }
