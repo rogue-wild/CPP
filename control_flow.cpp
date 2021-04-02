517be96cc7bf70a1c4f33634f6b0faaa1228f2fe
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-04-02 23:20:08 +0530

    .

--- control_flow.cpp
+++ control_flow.cpp
@@ -60,19 +60,22 @@ Output:
 
     //
 
-    //conditional operator
-    int a{10}, b{20};
-    int score{92};
-    int result{};
+    // //conditional operator
+    // int a{10}, b{20};
+    // int score{92};
+    // int result{};
 
-    result = (a > b) ? a : b;
-    cout << result << endl;
+    // result = (a > b) ? a : b;
+    // cout << result << endl;
 
-    result = (a < b ? (b - a) : (a - b));
-    cout << result << endl;
+    // result = (a < b ? (b - a) : (a - b));
+    // cout << result << endl;
 
-    result = (b != 0) ? (a / b) : 0;
-    cout << result << endl;
+    // result = (b != 0) ? (a / b) : 0; //avoiding here divide by zero error.
+    // cout << result << endl;
 
-    cout << ((score > 90) ? "Excellent" : "Good");
+    // cout << ((score > 90) ? "Excellent" : "Good");
+
+    //Looping
+     
 }
