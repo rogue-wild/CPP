852a8277cbe80f6f80120c3820b217f1e7ab4a31
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-04-24 18:07:30 +0530

    .

--- comp_prog/template.cpp
+++ comp_prog/template.cpp
@@ -1,32 +1,35 @@
 
-#include<iostream>
+#include <iostream>
 // #include <bits/stdc++.h>
-// #include<vector> 
+// #include<vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
 // using std::vector;
+// using namespace std;
 
 int answer{
 
-// some code goes here 
+	// some code goes here
 
 }
 
 ;
 
-int main() {
-	
-    // ios_base::sync_with_stdio(false);
-	// cin.tie(NULL);
+int main()
+{
+
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	// Read the test cases
-	int test_case;
-	cin>>test_case;	
-		while(test_case){
-            // call the function here
-            cout<<answer()<<endl;
-        }
+	int test_case{0};
+	cin >> test_case;
+	while (test_case)
+	{
+		// call the function here
+		cout << answer() << endl;
+	}
 	return 0;
 }
