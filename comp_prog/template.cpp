614b2a850dd93c21168c9c2f22b63574b9f345fa
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-04-24 17:50:37 +0530

    .

---
+++ comp_prog/template.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+// #include <bits/stdc++.h>
+// #include<vector> 
+
+using std::cin;
+using std::cout;
+using std::endl;
+// using std::vector;
+
+int answer{
+
+// some code goes here 
+
+}
+
+int main() {
+	
+    // ios_base::sync_with_stdio(false);
+	// cin.tie(NULL);
+
+	// Read the test cases
+	int test_case;
+	cin>>test_case;	
+		while(test_case){
+            // call the function here
+            cout<<answer()<<endl;
+        }
+	return 0;
+}
