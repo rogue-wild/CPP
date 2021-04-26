45b4d0fbbbff27062e84fd751401a7d0483763e1
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2021-04-26 18:16:47 +0530

    .

--- pepcoding(Level-1)/Getting-Started/printPrime.cpp
+++ pepcoding(Level-1)/Getting-Started/printPrime.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+
 using namespace std;
 
 //Code starts here
-    // Pepcoding Modules:- Data Structues and Algorithm[Beginner-1]
-    //  Basics of Programming, Getting Started
-    //*************************************************************
+// Pepcoding Modules:- Data Structues and Algorithm[Beginner-1]
+//  Basics of Programming, Getting Started
+//*************************************************************
 
 //Print All Primes Till N
-int main(int argc, char **argv){
+
+int main()
+{
     int low, high;
     cin >> low >> high;
 
-    //write your code here
+    bool prime{true};
 
+    for (int i{2}; i <= high; i++)
+    {
+        if (low % i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime == true)
+    {
+        cout << low << endl;
+        low++;
+    }
+
+    return 0;
 }
